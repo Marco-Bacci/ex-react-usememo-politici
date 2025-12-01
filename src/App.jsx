b071ecc2fe4ee73276063388128203b4281ce93a
Marco Bacci <marcobacci94@gmail.com>
2025-12-02 00:09:09 +0100

    Feat: milestone 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 function App() {
   const [politicians, setPoliticians] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter((poli) => {
+      return (
+        poli.name.toLowerCase().includes(search.toLowerCase()) ||
+        poli.biography.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+  }, [politicians, search]);
 
   useEffect(() => {
     fetch("http://localhost:3333/politicians")
@@ -10,9 +20,16 @@ function App() {
   }, []);
   console.log(politicians);
   return (
-    <div className="container">
+    <div className="container text-center">
+      <h1>LISTA DEI POLITICI</h1>
+      <input
+        type="text"
+        placeholder="ricerca per nome"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <div className="row">
-        {politicians.map((poli) => (
+        {filteredPoliticians.map((poli) => (
           <div className="col-4" key={poli.id}>
             <div className="card">
               <h2>{poli.name}</h2>
